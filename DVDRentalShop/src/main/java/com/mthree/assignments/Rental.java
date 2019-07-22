a3b0ccd73fcb4e58c8129a3a4b05e7d421c1fd3a
alumniJohnson12 <alumniJohnson12@10.0.103.24>
2019-07-22 13:38:47 -0400

    Added properties, getters, and setters for Staff and Rental Class

--- DVDRentalShop/src/main/java/com/mthree/assignments/Rental.java
+++ DVDRentalShop/src/main/java/com/mthree/assignments/Rental.java
@@ -1,5 +1,57 @@
 package com.mthree.assignments;
 
+import java.sql.Timestamp;
+
 public class Rental {
+	private long rentalId;
+	private Timestamp rentalDate;
+	private long inventoryId;
+	private long customerId;
+	private Timestamp returnDate;
+	private long staffId;
+	private Timestamp lastUpdate;
+	
+	public long getRentalId() {
+		return rentalId;
+	}
+	public void setRentalId(long rentalId) {
+		this.rentalId = rentalId;
+	}
+	public Timestamp getRentalDate() {
+		return rentalDate;
+	}
+	public void setRentalDate(Timestamp rentalDate) {
+		this.rentalDate = rentalDate;
+	}
+	public long getInventoryId() {
+		return inventoryId;
+	}
+	public void setInventoryId(long inventoryId) {
+		this.inventoryId = inventoryId;
+	}
+	public long getCustomerId() {
+		return customerId;
+	}
+	public void setCustomerId(long customerId) {
+		this.customerId = customerId;
+	}
+	public Timestamp getReturnDate() {
+		return returnDate;
+	}
+	public void setReturnDate(Timestamp returnDate) {
+		this.returnDate = returnDate;
+	}
+	public long getStaffId() {
+		return staffId;
+	}
+	public void setStaffId(long staffId) {
+		this.staffId = staffId;
+	}
+	public Timestamp getLastUpdate() {
+		return lastUpdate;
+	}
+	public void setLastUpdate(Timestamp lastUpdate) {
+		this.lastUpdate = lastUpdate;
+	}
 
 }
---
+++ DVDRentalShop/src/main/java/com/mthree/assignments/Staff.java
@@ -0,0 +1,77 @@
+package com.mthree.assignments;
+import java.sql.Timestamp;
+
+public class Staff {
+	private long staffId;
+	private String firstName;
+	private String lastName;
+	private long addressId;
+	private String email;
+	private long storeId;
+	private long active;
+	private String username;
+	private String password;
+	private Timestamp lastUpdate;
+	
+	public long getStaffId() {
+		return staffId;
+	}
+	public void setStaffId(long staffId) {
+		this.staffId = staffId;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public long getAddressId() {
+		return addressId;
+	}
+	public void setAddressId(long addressId) {
+		this.addressId = addressId;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public long getStoreId() {
+		return storeId;
+	}
+	public void setStoreId(long storeId) {
+		this.storeId = storeId;
+	}
+	public long getActive() {
+		return active;
+	}
+	public void setActive(long active) {
+		this.active = active;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public Timestamp getLastUpdate() {
+		return lastUpdate;
+	}
+	public void setLastUpdate(Timestamp lastUpdate) {
+		this.lastUpdate = lastUpdate;
+	}
+
+}
