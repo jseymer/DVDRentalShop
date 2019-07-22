08a156de7686d4c8b27ca548353e1115009933a6
David Solomon <djsolomon1421@gmail.com>
2019-07-22 16:11:58 -0400

    added film actor class

--- DVDRentalShop/src/main/java/com/mthree/assignments/FilmActor.java
+++ DVDRentalShop/src/main/java/com/mthree/assignments/FilmActor.java
@@ -1,5 +1,40 @@
 package com.mthree.assignments;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "Film_Actor")
 public class FilmActor {
+	
+	@Id
+	@Column(name = "Actor_ID")
+	private long actorID;
+	
+	@Column(name = "Film_ID")
+	private long filmID;
+	
+	public FilmActor(long actorID, long filmID) {
+		this.setActorID(actorID);
+		this.setFilmID(filmID);
+	}
+
+	public long getActorID() {
+		return actorID;
+	}
+
+	public void setActorID(long actorID) {
+		this.actorID = actorID;
+	}
+
+	public long getFilmID() {
+		return filmID;
+	}
+
+	public void setFilmID(long filmID) {
+		this.filmID = filmID;
+	}
 
 }
