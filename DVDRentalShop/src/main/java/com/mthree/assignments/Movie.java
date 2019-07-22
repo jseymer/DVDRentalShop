813af0c97d12327ca0125839811b0f93a4eed028
Alumni aj209 <jshen9085@gmail.com>
2019-07-22 14:16:58 -0400

    added setters and getters for movie

--- DVDRentalShop/src/main/java/com/mthree/assignments/Movie.java
+++ DVDRentalShop/src/main/java/com/mthree/assignments/Movie.java
@@ -1,6 +1,80 @@
 package com.mthree.assignments;
 
+import java.math.BigDecimal;
+import java.util.Date;
+
 public class Movie {
 	
-
+	private int filmId, year, rentDuration, length;
+	private String title, discription, rating, features;
+	private BigDecimal rate, replaceCost;
+	private Date lastUpdate;
+	
+	public int getFilmId() {
+		return filmId;
+	}
+	public void setFilmId(int filmId) {
+		this.filmId = filmId;
+	}
+	public int getYear() {
+		return year;
+	}
+	public void setYear(int year) {
+		this.year = year;
+	}
+	public int getRentDuration() {
+		return rentDuration;
+	}
+	public void setRentDuration(int rentDuration) {
+		this.rentDuration = rentDuration;
+	}
+	public int getLength() {
+		return length;
+	}
+	public void setLength(int length) {
+		this.length = length;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getDiscription() {
+		return discription;
+	}
+	public void setDiscription(String discription) {
+		this.discription = discription;
+	}
+	public String getRating() {
+		return rating;
+	}
+	public void setRating(String rating) {
+		this.rating = rating;
+	}
+	public String getFeatures() {
+		return features;
+	}
+	public void setFeatures(String features) {
+		this.features = features;
+	}
+	public BigDecimal getRate() {
+		return rate;
+	}
+	public void setRate(BigDecimal rate) {
+		this.rate = rate;
+	}
+	public BigDecimal getReplaceCost() {
+		return replaceCost;
+	}
+	public void setReplaceCost(BigDecimal replaceCost) {
+		this.replaceCost = replaceCost;
+	}
+	public Date getLastUpdate() {
+		return lastUpdate;
+	}
+	public void setLastUpdate(Date lastUpdate) {
+		this.lastUpdate = lastUpdate;
+	}
+	
 }
