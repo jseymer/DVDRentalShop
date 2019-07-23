219563899c5f6a0659de949f9a744464cbe9d286
Alumni aj209 <glaciersp@gmail.com>
2019-07-23 10:10:11 -0400

    added city class

---
+++ DVDRentalShop/src/main/java/com/mthree/assignments/City.java
@@ -0,0 +1,48 @@
+package com.mthree.assignments;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "City")
+public class City {
+	
+	@Id
+	@Column(name = "city_id")
+	private long cityId;
+	@Column(name = "city")
+	private String cityName;
+	@Column(name = "country_id")
+	private long countryId;
+	
+	
+	public City(long cityId, String cityName, long countryId) {
+		super();
+		this.cityId = cityId;
+		this.cityName = cityName;
+		this.countryId = countryId;
+	}
+	public long getCityId() {
+		return cityId;
+	}
+	public void setCityId(long cityId) {
+		this.cityId = cityId;
+	}
+	public String getCityName() {
+		return cityName;
+	}
+	public void setCityName(String cityName) {
+		this.cityName = cityName;
+	}
+	public long getCountryId() {
+		return countryId;
+	}
+	public void setCountryId(long countryId) {
+		this.countryId = countryId;
+	}
+	
+	
+
+}
